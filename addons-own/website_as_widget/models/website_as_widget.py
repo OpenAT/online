9fde3400694820eb1ddfe5b79fdcbd0ced750b29
Michael Karrer <michaelkarrer81@gmail.com>
2016-05-13 16:23:25 +0200

    [FIX] update fail must remove fields in next release

--- addons-own/website_as_widget/models/website_as_widget.py
+++ addons-own/website_as_widget/models/website_as_widget.py
@@ -3,32 +3,32 @@ __author__ = 'mkarrer'
 
 from openerp.osv import osv, orm, fields
 
+# DEPRICATED REMOVE IN NEXT RELEASE!
+class website_settings(osv.Model):
+    _inherit = 'website'
+    _columns = {
+        # Checkout Pages Headers
+        'global_redirect_url': fields.char(string='Global redirect URL',
+            help='Used if not called with aswidget=True and not from a configuration URL',
+                                           translate=True),
+        'global_configuration_urls': fields.char(string='Configuration URLS separated by ;',
+            help='Any URL other than a configuration URL or localhost will redirect to the global_redirect_url if set'),
+    }
 
-# class website_settings(osv.Model):
-#     _inherit = 'website'
-#     _columns = {
-#         # Checkout Pages Headers
-#         'global_redirect_url': fields.char(string='Global redirect URL',
-#             help='Used if not called with aswidget=True and not from a configuration URL',
-#                                            translate=True),
-#         'global_configuration_urls': fields.char(string='Configuration URLS separated by ;',
-#             help='Any URL other than a configuration URL or localhost will redirect to the global_redirect_url if set'),
-#     }
-#
-#
-# class website_config_settings(osv.osv_memory):
-#     _inherit = 'website.config.settings'
-#
-#     _columns = {
-#         'global_redirect_url': fields.related('website_id', 'global_redirect_url',
-#                                               type="char",
-#                                               string='Global redirect URL',
-#             help='Used if not called with aswidget=True and not from a configuration URL',
-#                                               translate=True),
-#         'global_configuration_urls': fields.related('website_id', 'global_configuration_urls', type="char",
-#             string='Configuration URLS separated by ;',
-#             help='Any URL other than a configuration URL or localhost will redirect to the global_redirect_url if set'),
-#     }
+# DEPRICATED REMOVE IN NEXT RELEASE!
+class website_config_settings(osv.osv_memory):
+    _inherit = 'website.config.settings'
+
+    _columns = {
+        'global_redirect_url': fields.related('website_id', 'global_redirect_url',
+                                              type="char",
+                                              string='Global redirect URL',
+            help='Used if not called with aswidget=True and not from a configuration URL',
+                                              translate=True),
+        'global_configuration_urls': fields.related('website_id', 'global_configuration_urls', type="char",
+            string='Configuration URLS separated by ;',
+            help='Any URL other than a configuration URL or localhost will redirect to the global_redirect_url if set'),
+    }
 
 
 class website_aswidget_domains(osv.Model):
