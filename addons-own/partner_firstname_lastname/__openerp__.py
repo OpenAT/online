c251e540ade13e4777dec4de40a71cecf19a6291
Michael Karrer <michaelkarrer81@gmail.com>
2016-06-14 09:08:09 +0200

    [ADD] partner_firstname_lastname set the name order to firstname lastname instead of the orignal lastname firstname

---
+++ addons-own/partner_firstname_lastname/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2010-2012 OpenERP s.a. (<http://openerp.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "FCOM partner_firstname_lastname",
+    'summary': """Change the default order of addon partner_firstname to firstname, lastname""",
+    'description': """
+
+partner_firstname_lastname
+==========================
+
+Change the default order of addon partner_firstname to firstname, lastname instead of lastname, firstname.
+
+    """,
+    'author': "Datadialog - Michael Karrer",
+    'website': "http://www.datadialog.net/",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'partner_firstname',
+    ],
+    'installable': True,
+    'data': [],
+}
---
+++ addons-own/partner_firstname_lastname/models.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+#    Author: Nicolas Bessi. Copyright Camptocamp SA
+#    Copyright (C)
+#       2014:       Agile Business Group (<http://www.agilebg.com>)
+#       2015:       Grupo ESOC <www.grupoesoc.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from openerp import api, fields, models
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def _get_computed_name(self, lastname, firstname):
+        # Fully Overwrite the original method to inverse the name
+        return u" ".join((p for p in (firstname, lastname) if p))
+
+    @api.model
+    def _get_inverse_name(self, name, is_company=False):
+        name = super(ResPartner, self)._get_inverse_name(name=name, is_company=is_company)
+        # Invert the original Result:
+        return {"lastname": name['firstname'], "firstname": name['lastname']}
