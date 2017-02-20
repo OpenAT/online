1361a42f365453f029389aa54abf4bf82a16de02
Michael Karrer <michaelkarrer81@gmail.com>
2017-02-20 19:22:24 +0100

    [FIX] logout method should not throw an exception if other kwargs are given than redirect

--- addons-own/auth_partner/controllers/main.py
+++ addons-own/auth_partner/controllers/main.py
@@ -5,9 +5,20 @@ from openerp.http import request
 from openerp.osv import orm
 from openerp.addons.auth_partner.fstoken_tools import fstoken_check
 from openerp.addons.web.controllers.main import login_and_redirect
+from openerp.addons.web.controllers.main import Session
 
 import logging
 _logger = logging.getLogger(__name__)
+
+
+# Extend the logout method to not throw an exception if other kwargs than redirect are given! e.g.: aswidget=True
+class SessionAuthPartner(Session):
+
+    # addons/web/controllers/main.py around line 865
+    # '/web/session/logout', type='http', auth="none"
+    @http.route()
+    def logout(self, redirect='/web', **kwargs):
+        return super(SessionAuthPartner, self).logout(redirect=redirect)
 
 
 # add fs_ptoken to the session if in URL or post args
@@ -37,3 +48,4 @@ class ir_http(orm.AbstractModel):
                                               redirect_url=request.httprequest.url)
 
         return response
+
