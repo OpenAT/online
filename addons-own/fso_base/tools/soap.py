5aaaf9f7522db920b2d04f3ac5df4deef7041f7e
Michael Karrer <michaelkarrer81@gmail.com>
2017-04-14 17:16:08 +0200

    [WIP] GetBPK

--- addons-own/fso_base/tools/soap.py
+++ addons-own/fso_base/tools/soap.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.fso_base.tools.validate import is_valid_url
 from openerp.tools.translate import _
-from jinja2 import Template
+from jinja2 import Template, Environment, FileSystemLoader
 import requests
 from requests import Session
-from lxml import etree
+import os
+import ntpath
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 def soap_request(url="", template="", http_header={}, crt_pem="", prvkey_pem="", **template_args):
-    # Check soap request URL
-    assert is_valid_url(url=url)[0], _('Soap Request URL %s not correct or DNS not found!') % url
+    # Validate the input
+    assert all((url, template)), _("Parameters missing! Required are url and template.")
+    assert is_valid_url(url=url)[0], _("Soap Request URL %s not valid and/or DNS resolution failed!") % url
+    if crt_pem or prvkey_pem:
+        assert all((crt_pem, prvkey_pem)), _("Certificate and private-key needed if one of them is given!")
 
     # Set the http header of the request (not the soap header)
     http_header = http_header or {
@@ -21,30 +25,33 @@ def soap_request(url="", template="", http_header={}, crt_pem="", prvkey_pem="",
     }
 
     # Render the soap request data
-    jinja2_template = Template(template)
-    post_data = jinja2_template.render(**template_args)
+    # Try to detect if the template is a file or directly the jinja data to render
+    if os.path.exists(template):
+        j2_env = Environment(loader=FileSystemLoader(os.path.abspath(os.path.dirname(template))))
+        soap_request_template = j2_env.get_template(ntpath.basename(template))
+        request_data = soap_request_template.render(**template_args)
+    else:
+        jinja2_template = Template(template)
+        request_data = jinja2_template.render(**template_args)
 
     # Create a Session Object (just like a regular UA e.g. Firefox or Chrome)
     session = Session()
     session.verify = True
-    session.cert = (crt_pem, prvkey_pem)
+    if crt_pem and prvkey_pem:
+        session.cert = (crt_pem, prvkey_pem)
 
     # Send the request (POST)
-    response = session.post(url, data=post_data, headers=http_header)
-
-    # Parse the request response (XML EXPECTED!) for nice error reporting in next step
-    parser = etree.XMLParser(remove_blank_text=True)
-    try:
-        response_etree = etree.fromstring(response.content, parser=parser)
-        response_string = etree.tostring(response_etree, pretty_print=True)
-    except:
-        response_string = response.content
-        logger.warning(_('Soap request response returned non valid xml:\n%s\n') % response.content)
+    # TODO: set some sort of timeout
+    response = session.post(url, data=request_data, headers=http_header)
 
     # Check for response errors
-    if response.status_code != requests.codes.ok:
-        logger.error(_('Soap request returned the http error code %s') % response.status_code)
-        logger.error(_('Soap request error response content:\n%s\n') % response_string)
-        response.raise_for_status()
+    # DISABLED: Because in most cases one would still want to process the response content to check error in
+    #           Soap XML Answer
+    # if response.status_code != requests.codes.ok:
+    #     logger.error(_('Soap request returned the http error code %s') % response.status_code)
+    #     logger.error(_('Soap request response content:\n%s\n') % response.content)
+    #     response.raise_for_status()
 
-    return response.content
+    # Return the Response
+    # HINT: There could still be an error in the response.content: Not all are nice and raise an http error
+    return response
--- addons-own/fso_con_zmr/models/models.py
+++ addons-own/fso_con_zmr/models/models.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
+import os
+from os.path import join as pj
 from openerp import api, models, fields
+from openerp.exceptions import ValidationError
+from openerp.tools.translate import _
+from openerp.addons.fso_base.tools.soap import soap_request
+from lxml import etree
+import requests
+import time
+import logging
+from collections import namedtuple
 
+logger = logging.getLogger(__name__)
+
+class RequestError(Exception):
+    pass
 
 class CompanyAustrianZMRSettings(models.Model):
     _inherit = 'res.company'
 
     # Basic Settings
-    Stammzahl = fields.Char(string="Firmenbuch-/ Vereinsregisternummer", help='Stammzahl e.g.: XZVR-123456789')
+    stammzahl = fields.Char(string="Firmenbuch-/ Vereinsregisternummer", help='Stammzahl e.g.: XZVR-123456789')
 
     # PVPToken userPrincipal
     pvpToken_userId = fields.Char(string="User ID (userId)")
@@ -22,22 +36,136 @@ class CompanyAustrianZMRSettings(models.Model):
 class ResPartnerZMRGetBPK(models.Model):
     _inherit = 'res.partner'
 
-    # Get the re
+    # This set of fields gets only updated if private and public bpk was returned successfully
+    BPKPrivate = fields.Char(string="BPK Private", readonly=True)
+    BPKPublic = fields.Char(string="BPK Public", readonly=True)
+    BPKRequestDate = fields.Datetime(string="BPK Request Date")
+    BPKRequestData = fields.Text(string="BPK Request Data")
+    BPKResponseData = fields.Text(string="BPK Response Data")
+
+    # This set of field gets updated by every bpk request with an error (or a missing bpk)
+    BPKErrorCode = fields.Char(string="BPK-Error Code")
+    BPKErrorText = fields.Text(string="BPK-Error Text")
+    BPKErrorRequestDate = fields.Datetime(string="BPK-Error Request Date")
+    BPKErrorRequestData = fields.Text(string="BPK-Error Request Data")
+    BPKErrorResponseData = fields.Text(string="BPK-Error Response Data")
+
     @api.model
-    def GetBPK(self, firstname=str(), lastname=str(), birthdate=str(), decrypted=True):
-        bpk = False
+    def request_bpk(self, firstname=str(), lastname=str(), birthdate=str()):
+        result = {'organization': "", 'request_data': "",
+                  'response_http_error_code': "", 'response_content': "", 'response_time_sec': "",
+                  'private_bpk': "", 'public_bpk': "", 'faultcode': "", 'faulttext': ""
+                  }
+
         # Validate input
-        # Get ZMR Data from default company
+        if not all((firstname, lastname, birthdate)):
+            raise ValidationError, _("Missing parameter! Mandatory are firstname, lastname and birthdate!")
+
+        # Get the request_data_template path
+        addon_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        soaprequest_templates = pj(addon_path, 'soaprequest_templates')
+        assert os.path.exists(soaprequest_templates), _("Folder soaprequest_templates not found at %s") \
+                                                      % soaprequest_templates
+        getbpk_template = pj(soaprequest_templates, 'GetBPK_j2template.xml')
+        assert os.path.exists(soaprequest_templates), _("GetBPK_j2template.xml not found at %s") \
+                                                      % getbpk_template
+
+        # Get current users company
         cmp = self.env.user.company
-        # Read jinja Templates
-        #     https://www.odoo.com/de_DE/forum/hilfe-1/question/is-there-a-way-to-get-the-location-to-your-odoo-and-to-create-new-files-in-your-custom-module-89677
-        # Render jinja templates with data
-        # Make soap request vis fso_base.tools.soap soap_request()
-        # Process soap answer
-        return bpk
+        result['organization'] = cmp.name
+
+        # HINT: In der Anwenderbeschreibung steht das man fuer private und oeffentliche BPK Anfragen eine
+        #       unterschiedliche Bereichskennung und target Bereichskennung verwenden muss. Dies wird jedoch
+        #       in der selben Beschreibung im Beispiel nicht verwendet sondern immer urn:publicid:gv.at:cdid+SA
+        #       und urn:publicid:gv.at:wbpk+XZVR+123456789 um beide BPKs zu bekommen?!?
+        #       Gefunden in: szr-3.0-anwenderdokumentation_v3_4.pdf
+        # Private  BPK (unverschluesselt: Kann zur Dublettenerkennung verwendet werden)
+        #              target_bereichskennung_privatebpk = "urn:publicid:gv.at:cdid+SA"
+        #              bereichskennung_privatebpk = "urn:publicid:gv.at:cdid+" + cmp.stammzahl.replace('-', '+')
+        # Public BPK (verschluesselt: Fuer die Uebermittlung der Spendensumme an das Bundesministerium fuer Finanzen)
+        #            target_bereichskennung_publicbpk = "urn:publicid:gv.at:ecdid+BMF+SA"
+        #            bereichskennung_publicbpk = "urn:publicid:gv.at:ecdid+" + cmp.stammzahl.replace('-', '+')
+        # Daher wird hier die Bereichskennung und Target Bereichskennung so wie im Beispiel angegeben verwendet und
+        # nicht wie weiter vorher im Text beschrieben.
+        #
+        #       BMF = Bundesministerium fuer Finanzen
+        #       SA = Steuern und Abgaben
+
+        start_time = time.time()
+        response = soap_request(url="https://pvawp.bmi.gv.at/at.gv.bmi.szrsrv-b/services/SZR",
+                                template=getbpk_template,
+                                crt_pem=cmp.crt_pem, prvkey_pem=cmp.prvkey_pem,
+                                GetBPK={
+                                    "VKZ": cmp.stammzahl,
+                                    "Target": {
+                                        "BereichsKennung": "urn:publicid:gv.at:cdid+SA",
+                                        "VKZ": "BMF"
+                                    },
+                                    "PersonInfo": {
+                                        "Person": {
+                                            "Name": {
+                                                "GivenName": firstname,
+                                                "FamilyName": lastname
+                                            },
+                                            "DateOfBirth": birthdate,
+                                        },
+                                    },
+                                    "BereichsKennung": "urn:publicid:gv.at:wbpk+" + cmp.stammzahl.replace('-', '+')
+                                },
+                                pvpToken={
+                                    "authorize": {
+                                        "role": ""
+                                    },
+                                    "authenticate": {
+                                        "userPrincipal": {
+                                            "cn": cmp.pvpToken_cn,
+                                            "gvGid": "AT:VKZ:" + cmp.stammzahl,
+                                            "userId": cmp.pvpToken_userId,
+                                            "gvOuId": cmp.pvpToken_gvOuId,
+                                            "gvSecClass": "2",
+                                            "ou": cmp.pvpToken_ou
+                                        },
+                                        "participantId": "AT:VKZ:" + cmp.stammzahl
+                                    }
+                                },
+                                )
+        assert response.content, _("Response has no content!")
+        # TODO: Check if the response object also stores the request data (rendered jinja template data)
+        # Todo: result['request_data'] = response.????
+        response_time = time.time() - start_time
+        result['response_time_sec'] = "%.3f" % response_time
+        result['response_content'] = response.content
+
+        # Process soap xml answer
+        parser = etree.XMLParser(remove_blank_text=True)
+        response_etree = etree.fromstring(response.content, parser=parser)
+        response_pprint = etree.tostring(response_etree, pretty_print=True)
+
+        # Validate xml response
+        if response.status_code != requests.codes.ok:
+            result['response_http_error_code'] = response.status_code
+            error_code = response_etree.find(".//faultcode")
+            result['faultcode'] = error_code.text if error_code is not None else result['faultcode']
+            error_text = response_etree.find(".//faultstring")
+            result['faulttext'] = error_text.text if error_text is not None else result['faulttext']
+            return result
+
+        # Extract BPKs
+        private_bpk = response_etree.find(".//GetBPKReturn")
+        result['private_bpk'] = private_bpk.text if private_bpk is not None else result['private_bpk']
+        public_bpk = response_etree.find(".//FremdBPK")
+        result['public_bpk'] = public_bpk.text if public_bpk is not None else result['public_bpk']
+
+        return result
 
     @api.multi
-    def PartnerGetBPK(self):
+    def setbpk(self):
         for partner in self:
-            # call partner.GetBPK() with correct Settings for Every Record and Update it
+            # TODO:
+            # run request_bpk for every partner
+            # update partner with the results
 
+    # TODO:
+    # Create an Action Button for setbpk
+    # Create a Server Action to run setbpk for every partner with missing bpk data or wrong bpk data and write date
+    #    newer than than last BPKRequestDate or BPKErrorDate
