e93b664c799fad78644e8d72f0331fe9d6c0f38f
Michael Karrer <michaelkarrer81@gmail.com>
2017-06-12 16:29:49 +0200

    [fso_con_zmr] new regex for firstname and lastname preparation as listed in issue #85

--- addons-own/fso_con_zmr/models/models.py
+++ addons-own/fso_con_zmr/models/models.py
@@ -19,33 +19,36 @@ logger = logging.getLogger(__name__)
 
 
 def clean_name(name, split=False):
+    # https://github.com/OpenAT/online/issues/64
     # HINT: Char fields are always unicode in odoo which is good therefore do not convert any Char fields with str()!
-    # ATTENTION: Flags like re.UNICODE do NOT work all the time!
-    #            Use (?u) instead in the start of any regex pattern!
+    # ATTENTION: Flags like re.UNICODE do NOT work all the time! Use (?u) instead in the start of any regex pattern!
 
-
+    # Replace u., und, & with +
+    name = re.sub(ur"(?u)[&]+", "+", name)
+    name = re.sub(ur"(?iu)\b(und|u)\b", "+", name)
+    # Remove right part starting with first +
+    name = name.split('+')[0]
     # Remove unwanted words case insensitive (?i)
     # HINT: This may leave spaces or dots after the words but these get cleaned later on anyway
-    name = re.sub(ur"(?iu)\b(fam|familie|sen|jun|und|u|persönlich|privat|c[/]o|anonym|e[.]u)\b", "", name)
+    name = re.sub(ur"(?iu)\b(fam|familie|sen|jun|persönlich|privat|c[/]o|anonym|e[.]u)\b", "", name)
     # Remove Numbers
     name = ''.join(re.findall(ur"(?u)[^0-9]+", name))
-    # Replace & and + with -
-    name = re.sub(ur"(?u)[&+]+", "-", name)
     # Keep only unicode alphanumeric-characters (keeps chars like e.g.: Öö ỳ Ṧ), dash and space
-    # HINT: This removes the left over dots from e.g.: Sen. or u.
+    # HINT: This removes the left over dots from e.g.: Sen. or e.u
     name = ''.join(re.findall(ur"(?u)[\w\- ]+", name))
     # Remove leading and trailing: whitespace and non alphanumeric characters
     name = re.sub(ur"(?u)^[\W]*|[\W]*$", "", name)
     # Replace multiple dashes with one dash
     name = re.sub(ur"(?u)-[\s\-]*-+", "-", name)
+    # Remove whitespace around dashes
+    name = re.sub(ur"(?u)\s*-\s*", "-", name)
     # Replace multiple spaces with one space
     name = re.sub(ur"(?u)\s\s+", " ", name)
-
     # Use only first word of name
     if split:
-        # Search from start until first non unicode alphanumeric-character is reached
+        # Search from start until first non unicode alphanumeric-character which is not a - is reached
         # HINT: Can only be space or dash at this point ;)
-        name = ''.join(re.findall(ur"(?u)^[\w]+", name))
+        name = ''.join(re.findall(ur"(?u)^[\w\-]+", name))
 
     return name
 
