7bb158851ee604731eeb51e22c13c7d6d8e9320f
Michael Karrer <michaelkarrer81@gmail.com>
2018-01-25 10:06:43 +0100

    [FIX] fso_sosync product_payment_interval.py remove active field because it was never set in the original model and change field type from Char to the correct type Text for the name field

--- addons-own/fso_sosync/models/product_payment_interval.py
+++ addons-own/fso_sosync/models/product_payment_interval.py
@@ -6,5 +6,4 @@ class ProductPaymentIntervalSosync(models.Model):
     _name = "product.payment_interval"
     _inherit = ["product.payment_interval", "base.sosync"]
 
-    name = fields.Char(sosync="True")
-    active = fields.Boolean(sosync="True")
+    name = fields.Text(sosync="True")
