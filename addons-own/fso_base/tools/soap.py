5a3b2f1828920a649b26f37f2a14fa20b57748bf
Michael Karrer <michaelkarrer81@gmail.com>
2018-01-25 09:59:17 +0100

    [IMP] fso_base create a sepearte method to render a jinja2 template in the soap.py file

--- addons-own/fso_base/tools/soap.py
+++ addons-own/fso_base/tools/soap.py
@@ -29,6 +29,27 @@ class GenericTimeoutError(Exception):
                                  "There may be a passphrase in your private key.")
 
 
+def render_template(template="", **template_args):
+    """
+    Renders a file as a jinja2 template based on the **template_args
+
+    :param template: template data or file system path to the template file
+    :param template_args: arguments used in the template (dicts)
+    :return: rendered template
+    """
+    # Try to detect if the template is a file or directly the jinja data to render
+    if os.path.exists(template):
+        j2_env = Environment(loader=FileSystemLoader(os.path.abspath(os.path.dirname(template))))
+        j2_env.filters['empty_if_exception_filter'] = empty_if_exception_filter
+        soap_request_template = j2_env.get_template(ntpath.basename(template))
+        request_data = soap_request_template.render(**template_args)
+    else:
+        jinja2_template = Template(template)
+        request_data = jinja2_template.render(**template_args)
+
+    return request_data
+
+
 # Multithreaded enabled timeout strategy
 # ATTENTION: !!! It may be that timeout_decorator crashes python! At least it does on Mac OSX and there are hints
 #                it also did it on linux !!!
@@ -36,10 +57,13 @@ class GenericTimeoutError(Exception):
 #                is a passphrase in the keys. So if we check for a passphrase we can be sure there will be no timeout
 #                by that! requests has its own timeout so no problem there either!
 #@timeout_decorator.timeout(18, use_signals=False, timeout_exception=GenericTimeoutError)
-def soap_request(url="", template="", http_header={}, crt_pem="", prvkey_pem="", **template_args):
-    # Validate the input
-    assert all((url, template)), _("Parameters missing! Required are url and template.")
+def soap_request(url="", template="", http_header={}, crt_pem="", prvkey_pem="", request_data=False, **template_args):
+
+    # Check for missing or malformed parameters
+    assert all((url, template or request_data)), _("Parameters missing! Required are url and template or request_data.")
     is_valid_url(url=url)
+
+    # Check the certificates if set
     if crt_pem or prvkey_pem:
         assert all((crt_pem, prvkey_pem)), _("Certificate- and Private-Key-File needed if one of them is given!")
         assert len(crt_pem) <= 255 and len(prvkey_pem) <= 255, _("Path to cert- or key-file is longer than 255 chars!")
@@ -54,16 +78,20 @@ def soap_request(url="", template="", http_header={}, crt_pem="", prvkey_pem="",
         'SOAPAction': ''
     }
 
-    # Render the soap request data
-    # Try to detect if the template is a file or directly the jinja data to render
-    if os.path.exists(template):
-        j2_env = Environment(loader=FileSystemLoader(os.path.abspath(os.path.dirname(template))))
-        j2_env.filters['empty_if_exception_filter'] = empty_if_exception_filter
-        soap_request_template = j2_env.get_template(ntpath.basename(template))
-        request_data = soap_request_template.render(**template_args)
+    # # Render the request data
+    # # Try to detect if the template is a file or directly the jinja data to render
+    # if os.path.exists(template):
+    #     j2_env = Environment(loader=FileSystemLoader(os.path.abspath(os.path.dirname(template))))
+    #     j2_env.filters['empty_if_exception_filter'] = empty_if_exception_filter
+    #     soap_request_template = j2_env.get_template(ntpath.basename(template))
+    #     request_data = soap_request_template.render(**template_args)
+    # else:
+    #     jinja2_template = Template(template)
+    #     request_data = jinja2_template.render(**template_args)
+    if request_data:
+        assert not template, "Request Data and a Template set!"
     else:
-        jinja2_template = Template(template)
-        request_data = jinja2_template.render(**template_args)
+        request_data = render_template(template=template, **template_args)
 
     # Create a Session Object (just like a regular UA e.g. Firefox or Chrome)
     session = Session()
@@ -76,14 +104,5 @@ def soap_request(url="", template="", http_header={}, crt_pem="", prvkey_pem="",
     request_data = request_data.encode('utf-8')
     response = session.post(url, data=request_data, headers=http_header, timeout=8)
 
-    # Check for response errors
-    # DISABLED: Because in most cases one would still want to process the response content to check error in
-    #           Soap XML Answer
-    # if response.status_code != requests.codes.ok:
-    #     logger.error(_('Soap request returned the http error code %s') % response.status_code)
-    #     logger.error(_('Soap request response content:\n%s\n') % response.content)
-    #     response.raise_for_status()
-
     # Return the Response
-    # HINT: There could still be an error in the response.content: Not all are nice and raise an http error
     return response
