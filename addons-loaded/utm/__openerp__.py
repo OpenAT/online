3e3676d8ee95d93d3154f2621ba170da32c97d5f
Michael Karrer <michaelkarrer81@gmail.com>
2018-07-30 18:01:23 +0200

    [IMP] link tracker backport from odoo 11

---
+++ addons-loaded/utm/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'UTM Trackers',
+    'category': 'Hidden',
+    'description': """
+Enable UTM trackers in shared links.
+=====================================================
+        """,
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/utm.xml',
+        'data/utm_data.xml'
+    ],
+    'demo': [],
+    'auto_install': False,
+}
---
+++ addons-loaded/utm/models/ir_http.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from openerp.http import request
+from openerp import models
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = 'ir.http'
+
+    #@classmethod
+    def get_utm_domain_cookies(self):
+        return request.httprequest.host
+
+    #@classmethod
+    def _set_utm(self, response):
+        if isinstance(response, Exception):
+            return response
+
+        domain = self.get_utm_domain_cookies()
+        for var, dummy, cook in request.env['utm.mixin'].tracking_fields():
+            if var in request.params and request.httprequest.cookies.get(var) != request.params[var]:
+                response.set_cookie(cook, request.params[var], domain=domain)
+        return response
+
+    #@classmethod
+    def _dispatch(self):
+        response = super(IrHttp, self)._dispatch()
+        return self._set_utm(response)
+
+    #@classmethod
+    def _handle_exception(self, exc):
+        response = super(IrHttp, self)._handle_exception(exc)
+        return self._set_utm(response)
