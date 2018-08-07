9b685cee1c222ff8120aa5a95e3dce9bd65fa388
N <nsteini@nikislaptop.localdomain>
2018-08-07 09:39:50 +0200

    [FIX] website_image_edit change openerp to disabled

---
+++ addons-own/website_image_edit/static/src/js/load_templates.js
@@ -0,0 +1,6 @@
+(function () {
+    'use strict';
+
+    openerp.website.add_template_file('/website_image_edit/static/src/xml/image_editor.xml');
+
+})();
---
+++ addons-own/website_image_edit/static/src/js/website_image_edit.js
@@ -0,0 +1,88 @@
+(function () {
+    'use strict';
+    console.log("website_image_editor")
+    var website = openerp.website;
+    var webEditor = website.editor;
+    console.log(webEditor);
+    console.log(website);
+
+//    website.Test = website.Widget.extend({
+//        console.log("test");
+//    });
+
+//
+////    website.website_image_edit = website.EditorBar.extend({
+////        edit: function () {
+////            /**
+////             * website/static/src(js/website.editor.js@339
+////             *
+////             * This is called when the Edit button in the editor toolbar is clicked
+////             *
+////             * Allows to disable and enable ckeditor editing by classes for snipped dom elements.
+////             * Since classes will survive "saving" this will be reapplied on every edit start whereas
+////             * the "contenteditable" attribute would be removed on save!
+////             */
+////            $( "body .ckediting_disabled" ).attr( "contenteditable", "false" );
+////            $( "body .ckediting_enabled" ).attr( "contenteditable", "true" );
+////
+////            // Show E-Mail settings in edit mode by default
+////            $( "#email_template_settings" ).collapse('show');
+////            $( "#email_template_settings" ).addClass('in');
+////
+////            return this._super.apply(this, arguments);
+////        }
+////    });
+//
+//    // another try
+//    website.editor.RTELinkDialog = website.editor.RTELinkDialog.extend({
+//
+//        bind_data: function () {
+//             console.log('bind_data() image edit');
+//
+//            var classes = null;
+//            classes = this.element && this.element.getAttribute("class") || '';
+//            // console.log('bind_data() this.element.class: ' + classes);
+//
+//            // search for class in classes
+//            var donottrack = classes.indexOf('link-donottrack') !== -1;
+//            var withtoken  = classes.indexOf('link-withtoken') !== -1;
+//
+//            // set input fields in link dialog
+//            this.$("input[class='link-donottrack']").prop('checked', donottrack);
+//            this.$("input[class='link-withtoken']").prop('checked', withtoken);
+//
+//            var result = null;
+//            result = this._super();
+//
+//            return result;
+//        },
+//
+//        make_link: function (url, new_window, label, classes) {
+//            // console.log('make_link(): ' + classes);
+//            classes = classes.replace(/undefined/g, '') || '';
+//            classes = classes.replace(/\s{2,}/g, ' ').trim();
+//
+//            // do not track
+//            if (this.$("input[class='link-donottrack']").prop("checked")) {
+//                classes += ' link-donottrack';
+//            }
+//            else {
+//                classes = classes.replace(/link-donottrack/g, '')
+//            }
+//
+//            // with token
+//            if (this.$("input[class='link-withtoken']").prop("checked")) {
+//                classes += ' link-withtoken';
+//            }
+//            else {
+//                classes = classes.replace(/link-withtoken/g, '')
+//            }
+//
+//            classes = classes.replace(/\s{2,}/g, ' ').trim();
+//            // console.log('make_link() after: ' + classes);
+//            return this._super(url, new_window, label, classes);
+//        },
+//
+//    })
+
+})();
