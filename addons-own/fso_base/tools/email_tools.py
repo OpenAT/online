d5cc6d1e953dfca95fef52ca90b5c20b12b29607
Michael Karrer <michaelkarrer81@gmail.com>
2018-08-10 16:53:22 +0200

    [WIP] fso_con_zmr better status messages for scheduled donation report submission

--- addons-own/fso_base/tools/email_tools.py
+++ addons-own/fso_base/tools/email_tools.py
@@ -7,6 +7,10 @@ from dateutil import tz
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.header import Header
+
+import urllib
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -20,35 +24,42 @@ def send_internal_email(odoo_env_obj=None, fromaddr='', toaddr='', subject='', b
     if odoo_env_obj is not None:
         try:
             dbname = "%s" % odoo_env_obj.cr.dbname
-            user = "%s" % odoo_env_obj.user.name
+            # user = "%s" % odoo_env_obj.user.name
             now = dt.now(tz=tz.gettz('Europe/Vienna'))
             nowstr = now.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
 
             fromaddr = "fson-%s@datadialog.net" % dbname if not fromaddr else fromaddr
             toaddr = "admin@datadialog.net" if not toaddr else toaddr
+
             subject = "FSON[%s] %s: %s" % (dbname, nowstr, subject)
+            if isinstance(subject, str):
+                subject = subject.decode('utf8')
+
             body = subject if not body else body
+            if isinstance(body, str):
+                body = body.decode('utf8')
+
         except Exception as e:
             logger.warning("send_internal_email() Could not get instance info!\n%s" % repr(e))
+            return False
 
     try:
-        # Prepare e-mail
-        msg = MIMEMultipart()
+        # Prepare email text
+        msg = MIMEText(body.encode('utf-8'), 'plain', 'utf-8')
         msg['From'] = fromaddr
         msg['To'] = toaddr
-        msg['Subject'] = subject
-        msg.attach(MIMEText(body, 'plain'))
+        msg['Subject'] = Header(subject, 'utf-8')
+        text = msg.as_string()
 
         # Send e-mail
         server = smtplib.SMTP('192.168.37.1', 25)
         # server.starttls()
         # server.login(fromaddr, "YOUR PASSWORD")
-        text = msg.as_string()
+        logger.info("send_internal_email() Sending e-mail: %s" % text)
         server.sendmail(fromaddr, toaddr, text)
         server.quit()
+        return True
 
     except Exception as e:
-        try:
-            logger.error("Could not send status e-mail!\n---\n%s\n---\n%s" % (text, repr(e)))
-        except:
-            logger.error("Could not send status e-mail! %s" % subject)
+        logger.error("Could not send status e-mail!\n---\n%s\n---\n" % repr(e))
+        return False
