61966189afabf8d43af2a4a65fa6749f3e3aa587
Michael Karrer <michaelkarrer81@gmail.com>
2018-11-25 16:22:42 +0100

    [REM] mass_mailing_partner removed from oca and moved to addons own to disable computed fields for update speed

---
+++ addons-own/mass_mailing_partner/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# © 2015 Antonio Espinosa <antonioea@antiun.com>
+# © 2015 Javier Iniesta <javieria@antiun.com>
+# © 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Link partners with mass-mailing",
+    "version": "8.0.2.1.0",
+    "author": "Tecnativa, "
+              "Antiun Ingeniería S.L., "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Marketing",
+    "depends": [
+        'mass_mailing',
+    ],
+    "post_init_hook": "post_init_hook",
+    'data': [
+        'views/mail_mail_statistics_view.xml',
+        'views/mail_mass_mailing_contact_view.xml',
+        'views/mail_mass_mailing_view.xml',
+        'views/res_partner_view.xml',
+        'wizard/partner_mail_list_wizard.xml'
+    ],
+    "installable": True,
+}
