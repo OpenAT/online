d2c22dd0213ca3bf87a2f8f3bd2169cea532ef47
Michael Karrer <michaelkarrer81@gmail.com>
2019-01-21 15:55:35 +0100

    [IMP] fso_base_website better update security for custom robots.txt additions and better defaults that can be updated by xml

---
+++ addons-own/fso_base_website/controller/main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from openerp import http
+from openerp.http import request
+
+import logging
+logger = logging.getLogger(__name__)
+
+from openerp.addons.website.controllers.main import Website
+
+
+class WebsiteFsoBase(Website):
+
+    # Overwrite original robots controller ('/robots.txt') to use our template and because of 'noupdate' in our xml!
+    @http.route()
+    def robots(self):
+        logger.info("Rendering alternative robots.txt from fso_base_website!")
+        cr, uid, context = request.cr, request.uid, request.context
+
+        website_id = request.registry['website'].search(cr, uid, [], limit=1, context=context)
+        website = request.registry['website'].browse(cr, uid, website_id[0], context=context)
+
+        return request.render('fso_base_website.robots_txt_template',
+                              {'url_root': request.httprequest.url_root,
+                               'robots_txt': website.robots_txt if website else '',
+                               },
+                              mimetype='text/plain')
--- addons-own/fso_base_website/models/website.py
+++ addons-own/fso_base_website/models/website.py
@@ -20,17 +20,25 @@ from openerp import models, fields, api
 class Website(models.Model):
     _inherit = 'website'
 
-    @api.model
-    def _get_robots_view(self):
-        return self.env.ref('website.robots')
+    # @api.model
+    # def _get_robots_view(self):
+    #     return self.env.ref('fso_base_website.robots_txt_view')
 
-    RobotsViewId = fields.Many2one(comodel_name="ir.ui.view", string="robots.txt id", default=_get_robots_view,
-                                   readonly=True)
-    RobotsArch = fields.Text(related="RobotsViewId.arch", string="robots.txt")
+    # robots_view_id = fields.Many2one(comodel_name="ir.ui.view", string="robots ir.ui.view id",
+    #                                  default=lambda self: self.env.ref('fso_base_website.robots_txt_view'),
+    #                                  readonly=True)
+    # robots_view_arch_field = fields.Text(related="robots_view_id.arch", string="robots.txt")
 
+    # New Solution
+    robots_txt = fields.Text(string="robots.txt Extras",
+                             help="Add custom instructions to the top of the robots.txt file!")
+
+    # TODO: !!! USE LOWERCASE FIELD NAMES !!!
     PublicPartnerNoSubscribe = fields.Boolean(related="partner_id.no_subscribe",
                                               string="Public Partner: Do not add as follower")
     PublicPartnerTimezone = fields.Selection(related="partner_id.tz",
                                              string="Public Partner: Timezone")
+    # END TODO
 
     google_analytics_script = fields.Text(string="Google Analytics Script")
+
