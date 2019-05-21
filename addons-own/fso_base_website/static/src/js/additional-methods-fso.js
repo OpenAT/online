60995c188d1765da51bc7ac2a5e798bee9f74bf3
Michael Karrer <michaelkarrer81@gmail.com>
2019-05-21 13:11:39 +0200

    [ADD] fso_base_website new frontend validation rule floatDEmin

--- addons-own/fso_base_website/static/src/js/additional-methods-fso.js
+++ addons-own/fso_base_website/static/src/js/additional-methods-fso.js
@@ -7,7 +7,7 @@
  * Released under the MIT license
  */
 // Add a new Valdiator for German Dates
-$.validator.addMethod("dateDEBirthdate", function (value, element) {
+$.validator.addMethod("dateDEBirthdate", function (value, element, params) {
     // Use moment.js to validate the date input
     // HINT: If nothing is passed to moment#isBefore, it will default to the current time.
     var test = moment(value, 'DD.MM.YYYY', 'de', true).isBetween('1900-01-01', moment().add(1, 'd'), 'year');
@@ -21,4 +21,20 @@ $.validator.addMethod("dateDEBirthdate", function (value, element) {
 $.validator.addMethod("floatDE", function (value, element) {
     // HINT: .optional() is there to return true as long as the input is empty
     return this.optional( element ) || /^[0-9]+[,]*[0-9]*$/.test( value );
+}, $.validator.messages.number);
+
+$.validator.addMethod("floatDEmin", function (value, element, params) {
+    // console.log('floatDEmin');
+    // HINT: .optional() is there to return true as long as the input is empty
+    if (!(this.optional( element ) || /^[0-9]+[,]*[0-9]*$/.test( value ))) {
+        // console.log('WRONG FORMAT');
+        return false
+    }
+
+    var value_float = parseFloat(value.replace(/,/g, "."));
+    var compare_float = parseFloat(params);
+    // console.log(value_float);
+    // console.log(compare_float);
+    return this.optional( element ) || value_float >= compare_float;
+
 }, $.validator.messages.number);
