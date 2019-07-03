e44f2649ea9e18d285f2fcda137ee160022d5adb
SteiningerNikolaus <steiningerN@gmx.at>
2019-07-03 09:30:35 +0200

    fso_website_email: code cleanup

--- addons-own/fso_website_email/static/src/js/delete_template.js
+++ addons-own/fso_website_email/static/src/js/delete_template.js
@@ -1,22 +1,14 @@
 (function () {
     'use strict';
-    console.log('delete template')
-
-    console.log(openerp.qweb)
 
     var templates = document.getElementsByClassName('fso_wem_delete_template');
-    console.log(templates)
 
     for (var i = 0; i < templates.length; i++) {
-        console.log(templates[i].id)
         templates[i].addEventListener("click", try_delete_template);
     }
 
     function try_delete_template (e) {
-        console.log('clicked')
-        console.log(e.path[0].id)
         var template_id = e.path[0].id;
-        console.log(template_id)
 
         $('#email_templates').after(
             openerp.qweb.render(
@@ -27,15 +19,12 @@
         warning.style.display = 'block';
 
         var cancelButton = document.getElementsByClassName('close_fso_wem_modal');
-        console.log(cancelButton)
         for (var i = 0; i < cancelButton.length; i++) {
-            console.log(cancelButton[i].id)
             cancelButton[i].addEventListener("click", cancel_delete);
         }
     }
 
     function cancel_delete () {
-        console.log('cancale')
         var toClose = document.getElementById('fso_wem_delete_modal');
         toClose.style.display = "none";
         $('#fso_wem_delete_modal').remove();
