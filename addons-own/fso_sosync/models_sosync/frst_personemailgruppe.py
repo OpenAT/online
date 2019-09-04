08863e3059c227b2a5475bcceafc4cc1687a8053
Michael Karrer <michaelkarrer81@gmail.com>
2019-09-04 17:11:59 +0200

    [FIX] fso_sosync add abstract fields like state to the group bridge models

--- addons-own/fso_sosync/models_sosync/frst_personemailgruppe.py
+++ addons-own/fso_sosync/models_sosync/frst_personemailgruppe.py
@@ -8,6 +8,12 @@ class FRSTPersonEmailGruppeSosync(models.Model):
 
     zgruppedetail_id = fields.Many2one(sosync="True")
     frst_personemail_id = fields.Many2one(sosync="True")
+
+    # From abstract model frst.gruppestate
+    state = fields.Selection(sosync="True")
     steuerung_bit = fields.Boolean(sosync="True")
     gueltig_von = fields.Date(sosync="True")
     gueltig_bis = fields.Date(sosync="True")
+    bestaetigt_am_um = fields.Datetime(sosync="True")
+    bestaetigt_typ = fields.Selection(sosync="True")
+    bestaetigt_herkunft = fields.Char(sosync="True")
