a78f86e80755f3e3114f95740d9759a27f79943d
Michael Karrer <michaelkarrer81@gmail.com>
2019-10-21 14:49:17 +0200

    [FIX] website_sale_categories show one_page_checkout option in backend view

---
+++ addons-archiv/mass_mailing_partner/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
+from . import wizard
+
+# DISABLED BY MIKE
+#from .hooks import post_init_hook
