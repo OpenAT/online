29bcd0b0062a3a430ce52e32536266ee92db3d8a
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2020-12-15 18:41:44 +0100

    [IMP] Validate swagger spec url

--- addons-own/swagger_libs/controllers/swagger_ui.py
+++ addons-own/swagger_libs/controllers/swagger_ui.py
@@ -3,6 +3,7 @@
 from openerp import http
 from openerp.http import request
 
+import validators
 import datetime
 
 from openerp.tools.mail import html_sanitize
@@ -10,15 +11,26 @@ import urllib
 
 
 class SwaggerUIDist(http.Controller):
+    @staticmethod
+    def api_url_valid(api_url):
+        return api_url\
+               and api_url.startswith(request.httprequest.host_url)\
+               and validators.url(api_url)
+
+    def raise_on_invalid_api_url(self, api_url):
+        if not self.api_url_valid(api_url):
+            raise Exception("swagger_spec_url not allowed")
 
     @http.route(['/swagger-ui/index.html'], type="http", auth="user", website=True)
     def swagger_ui(self, swagger_spec_url="https://petstore.swagger.io/v2/swagger.json"):
+        self.raise_on_invalid_api_url(swagger_spec_url)
         return request.render('swagger_libs.swagger_ui_index_html',
                               {'swagger_spec_url': swagger_spec_url}
                               )
 
     @http.route(['/swagger-editor/index.html'], type="http", auth="user", website=True)
     def swagger_editor(self, swagger_spec_url="https://petstore.swagger.io/v2/swagger.json"):
+        self.raise_on_invalid_api_url(swagger_spec_url)
         return request.render('swagger_libs.swagger_editor_index_html',
                               {'swagger_spec_url': swagger_spec_url}
                               )
