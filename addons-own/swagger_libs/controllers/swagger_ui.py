71d2d68e86041e422c6154dbb5c95a341cc6045a
Michael Karrer <michaelkarrer81@gmail.com>
2020-12-15 16:38:44 +0100

    [WIP] swagger_libs

---
+++ addons-own/swagger_libs/controllers/swagger_ui.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from openerp import http
+from openerp.http import request
+
+import datetime
+
+from openerp.tools.mail import html_sanitize
+import urllib
+
+
+class SwaggerUIDist(http.Controller):
+
+    @http.route(['/swagger-ui/index.html'], type="http", auth="user", website=True)
+    def swagger_ui(self, swagger_spec_url="https://petstore.swagger.io/v2/swagger.json"):
+        return request.render('swagger_libs.swagger_ui_index_html',
+                              {'swagger_spec_url': swagger_spec_url}
+                              )
