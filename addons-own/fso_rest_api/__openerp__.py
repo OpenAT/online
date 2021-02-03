22916a64c725a3cd1cc5ac09b77f5849b9a5e445
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2021-02-03 10:31:22 +0100

    [ADD] frst.zgruppe data file

--- addons-own/fso_rest_api/__openerp__.py
+++ addons-own/fso_rest_api/__openerp__.py
@@ -52,5 +52,6 @@ If you need to make local or manual changes create a new integration!
         'data/frst_rest_api.xml',
         'data/res_partner.xml',
         'data/frst_zverzeichnis.xml',
+        'data/frst_zgruppe.xml',
     ],
 }
