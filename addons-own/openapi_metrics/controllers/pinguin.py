a8ada9ceada96ab711f0d719f8000db440c406da
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2021-02-17 16:03:00 +0100

    [FIX] Upsert error on older postgreSQL versions

--- addons-own/openapi_metrics/controllers/pinguin.py
+++ addons-own/openapi_metrics/controllers/pinguin.py
@@ -10,12 +10,77 @@ from openerp.http import request
 _logger = logging.getLogger(__name__)
 _super_create_log_record = pinguin.create_log_record
 
+_SQL_UPSERT = """
+    INSERT INTO openapi_http_metric AS m (
+        namespace_id,
+        namespace_name,
+        day,
+        model,
+        request_count,
+        create_uid,
+        create_date
+        )
+    VALUES (
+        %(namespace_id)s,           -- namespace_id
+        %(namespace_name)s,         -- namespace_name
+        current_date,               -- day
+        %(model)s,                  -- model
+        1,                          -- request_count
+        %(uid)s,                    -- create_uid
+        now() at time zone 'utc'    -- create_date
+        )
+    ON CONFLICT (namespace_id, day, model) DO UPDATE SET
+        request_count = m.request_count + 1,
+        write_date = now() at time zone 'utc',
+        write_uid = %(uid)s;
+    """
+
+_SQL_UPSERT_FALLBACK = """
+    WITH upsert AS (
+        UPDATE
+            openapi_http_metric
+        SET
+            request_count = request_count + 1
+            , write_date = now() at time zone 'utc'
+            , write_uid = %(uid)s
+        WHERE
+            namespace_id = %(namespace_id)s
+            AND day = current_date
+            AND model = %(model)s
+        RETURNING *
+    )
+    INSERT INTO openapi_http_metric (
+        namespace_id,
+        namespace_name,
+        day,
+        model,
+        request_count,
+        create_uid,
+        create_date
+        )
+    SELECT
+        %(namespace_id)s,           -- namespace_id
+        %(namespace_name)s,         -- namespace_name
+        current_date,               -- day
+        %(model)s,                  -- model
+        1,                          -- request_count
+        %(uid)s,                    -- create_uid
+        now() at time zone 'utc'    -- create_date
+    FROM
+        upsert
+    WHERE
+        NOT EXISTS (SELECT * FROM upsert);
+    """
+
 
 def create_log_and_metrics_record(**kwargs):
     test_mode = request.registry.test_cr
 
     if not test_mode:
         with openerp.registry(request.session.db).cursor() as cr:
+            # Upsert supported in postgreSQL 9.5+
+            upsert_supported = cr.connection.server_version >= 90500
+
             # Custom new cursor, so metrics can be written even if
             # parent cursor has errors.
             _create_metric_record(
@@ -23,38 +88,18 @@ def create_log_and_metrics_record(**kwargs):
                 namespace_id=kwargs["namespace_id"],
                 namespace_name=kwargs["namespace"],
                 uid=request.session.uid,
-                model=kwargs["model"])
+                model=kwargs["model"],
+                upsert_supported=upsert_supported)
 
     return _super_create_log_record(**kwargs)
 
 
-def _create_metric_record(cr, namespace_id, namespace_name, uid, model):
+def _create_metric_record(cr, namespace_id, namespace_name, uid, model, upsert_supported):
     # Use raw SQL in order to utilize postgres UPSERT,
     # for fastest possible insert/update.
-    query = """
-        INSERT INTO openapi_http_metric AS m (
-            namespace_id,
-            namespace_name,
-            day,
-            model,
-            request_count,
-            create_uid,
-            create_date
-            )
-        VALUES (
-            %(namespace_id)s,           -- namespace_id
-            %(namespace_name)s,         -- namespace_name
-            current_date,               -- day
-            %(model)s,                  -- model
-            1,                          -- request_count
-            %(uid)s,                    -- create_uid
-            now() at time zone 'utc'    -- create_date
-            )
-        ON CONFLICT (namespace_id, day, model) DO UPDATE SET
-            request_count = m.request_count + 1,
-            write_date = now() at time zone 'utc',
-            write_uid = %(uid)s;
-        """
+    _logger.debug("Counting metric using %s" % "native upsert" if upsert_supported else "fallback upsert")
+    query = _SQL_UPSERT if upsert_supported else _SQL_UPSERT_FALLBACK
+
     cr.execute(
         query,
         {
