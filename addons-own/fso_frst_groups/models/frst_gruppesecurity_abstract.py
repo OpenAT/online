f0e14619dae98d974ae262668bec38a002acdec1
Michael Karrer <michaelkarrer81@gmail.com>
2021-04-13 19:15:46 +0200

    [WIP] complete rewrite for frst_gruppesecurity_abstract to include the state - rework of the tests

--- addons-own/fso_frst_groups/models/frst_gruppesecurity_abstract.py
+++ addons-own/fso_frst_groups/models/frst_gruppesecurity_abstract.py
@@ -12,87 +12,75 @@ class FRSTGruppeSecurity(models.AbstractModel):
     """ Check if the modification of group subscriptions is allowed based on the zGruppe field
     'gui_gruppen_bearbeiten_moeglich'.
 
+    The naming is missleading! Should be named frst_subscription_security_abstract!
+
     Usage:
-    Add this to group_subscritpion_models like personemailgruppe or persongruppe.
+    Add this to group_subscription_models like personemailgruppe or persongruppe.
     ATTENTION: It must be ensured that the group field is called 'zgruppedetail_id' or this will fail!
     """
     _name = "frst.gruppesecurity"
 
     @api.model
-    def check_gui_gruppen_bearbeiten_moeglich(self, zgruppedetail, user=None):
-        gui_gruppen_bearbeiten_moeglich = zgruppedetail.zgruppe_id.gui_gruppen_bearbeiten_moeglich
-        if user is None:
-            user = self.env.user
-            if user and not gui_gruppen_bearbeiten_moeglich and not user.has_group('base.sosync'):
-                raise AccessError(_("You must be in the sosyncer-user-group to create or edit subscriptions "
-                                    "for this Fundraising Studio group."))
+    def check_gui_gruppen_bearbeiten_moeglich(self):
+        protected = self.filtered(lambda r: not r.zgruppedetail_id.zgruppe_id.gui_gruppen_bearbeiten_moeglich)
+
+        if protected and self.env.user and not self.env.user.has_group('base.sosync'):
+            raise AccessError(_("You must be in the sosyncer-user-group to create, edit or delete the subscription(s):"
+                                " %s") % protected)
 
     @api.model
-    def check_nur_eine_gruppe_anmelden(self, zgruppedetail, target_record_id=None):
-        subscription_sudo_env = self.sudo()
-
-        nur_eine_gruppe_anmelden = zgruppedetail.zgruppe_id.nur_eine_gruppe_anmelden
-        if not nur_eine_gruppe_anmelden:
-            return
-
-        other_groups_in_folder = zgruppedetail.zgruppe_id.zgruppedetail_ids - zgruppedetail
-        if not other_groups_in_folder:
-            return
-
+    def check_nur_eine_gruppe_anmelden(self):
+        # Determine group subscription target record field
         if self._name == 'frst.persongruppe':
-            target_record_field = 'partner_id'
+            group_target_field = 'partner_id'
         elif self._name == 'frst.personemailgruppe':
-            target_record_field = 'frst_personemail_id'
+            group_target_field = 'frst_personemail_id'
         else:
             raise ValueError("Unknown model:  %s!" % self._name)
-        if target_record_id is None:
-            self.ensure_one()
-            target_record_id = self[target_record_field].id
-        other_subscritpions = subscription_sudo_env.search([
-                (target_record_field, '=', target_record_id),
-                ('zgruppedetail_id.id', 'in', other_groups_in_folder.ids)
-            ],
-            limit=1)
 
-        if other_subscritpions:
-            raise ValidationError("Only one subscription is allowed for groups in the group folder %s! Existing"
-                                  "subscription: %s" % (zgruppedetail.zgruppe_id, other_subscritpions.ids))
+        # Filter out the subscriptions to check
+        subscriptions_to_check = self.filtered(lambda r: r.zgruppedetail_id.zgruppe_id.nur_eine_gruppe_anmelden)
+        if not subscriptions_to_check:
+            return
+
+        # Search for other active subscriptions for the group subscription target record (e.g. a res.partner)
+        for r in subscriptions_to_check:
+            group_target_id = r[group_target_field].id
+            groups_in_folder = r.zgruppedetail_id.zgruppe_id.zgruppedetail_ids
+            active = self.sudo().search([
+                (group_target_field, '=', group_target_id),
+                ('zgruppedetail_id.id', 'in', groups_in_folder.ids),
+                ('state', 'not in', ['unsubscribed', 'expired'])
+                ],
+                limit=2)
+            if len(active) >= 2:
+                raise ValidationError("Only one group can have an active subscription for groups in the group folder %s"
+                                      " ! Active subscriptions: %s" % (r.zgruppedetail_id.zgruppe_id,
+                                                                       active))
+            # TODO: Performance optimization: We may remove all other subscriptions for this subscription target
+            #                                 in subscriptions_to_check if they are for groups_in_folder
 
     @api.model
     def create(self, vals):
-        zgruppedetail_id = vals.get('zgruppedetail_id', None)
-        if zgruppedetail_id:
-            zgruppedetail = self.env['frst.zgruppedetail'].browse(zgruppedetail_id)
+        record = super(FRSTGruppeSecurity, self).create(vals)
+        if record:
+            record.check_gui_gruppen_bearbeiten_moeglich()
+            record.check_nur_eine_gruppe_anmelden()
 
-            # check_gui_gruppen_bearbeiten_moeglich
-            self.check_gui_gruppen_bearbeiten_moeglich(zgruppedetail)
-
-            # check_nur_eine_gruppe_anmelden
-            if self._name == 'frst.persongruppe':
-                target_record_field = 'partner_id'
-            elif self._name == 'frst.personemailgruppe':
-                target_record_field = 'frst_personemail_id'
-            else:
-                raise ValueError("Unknown model:  %s!" % self._name)
-            if vals.get(target_record_field):
-                self.check_nur_eine_gruppe_anmelden(zgruppedetail, vals[target_record_field])
-
-        return super(FRSTGruppeSecurity, self).create(vals)
+        return record
 
     @api.multi
     def write(self, vals):
         res = super(FRSTGruppeSecurity, self).write(vals)
         if res:
-            protected = self.filtered(lambda r: not r.zgruppedetail_id.zgruppe_id.gui_gruppen_bearbeiten_moeglich)
-            for r in protected:
-                r.check_gui_gruppen_bearbeiten_moeglich(r.zgruppedetail_id)
-                r.check_nur_eine_gruppe_anmelden(r.zgruppedetail_id)
+            self.check_gui_gruppen_bearbeiten_moeglich()
+            self.check_nur_eine_gruppe_anmelden()
+
         return res
 
     @api.multi
     def unlink(self):
-        protected = self.filtered(lambda r: not r.zgruppedetail_id.zgruppe_id.gui_gruppen_bearbeiten_moeglich)
-        for r in protected:
-            r.check_gui_gruppen_bearbeiten_moeglich(r.zgruppedetail_id)
-            r.check_nur_eine_gruppe_anmelden(r.zgruppedetail_id)
+        for r in self:
+            r.check_gui_gruppen_bearbeiten_moeglich()
+
         return super(FRSTGruppeSecurity, self).unlink()
