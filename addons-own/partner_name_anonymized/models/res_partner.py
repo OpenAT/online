05f1652b40abda190ca336644bafe389cd78f092
Michael Karrer <michaelkarrer81@gmail.com>
2021-07-01 09:36:38 +0200

    [ADD] partner_name_anonymized

---
+++ addons-own/partner_name_anonymized/models/res_partner.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from openerp import api, fields, models
+from openerp.tools.translate import _
+
+
+class ResPartnerTwitch(models.Model):
+    _inherit = 'res.partner'
+
+    name_anonymized = fields.Char(string='Name Anonymized', compute="compute_name_anonymized")
+
+    @api.depends('firstname', 'lastname')
+    def compute_name_anonymized(self):
+        for partner in self:
+            if partner.firstname and partner.lastname and len(partner.lastname) > 4:
+                partner.name_anonymized = "%s %s." % (partner.firstname, partner.lastname[:1])
+            else:
+                firstname = partner.firstname[:1] + '.' if partner.firstname else ''
+                lastname = partner.lastname[:1] + '.' if partner.lastname else ''
+                if firstname or lastname:
+                    partner.name_anonymized = "%s %s" % (firstname, lastname)
+                else:
+                    partner.name_anonymized = _("Anonym.")
