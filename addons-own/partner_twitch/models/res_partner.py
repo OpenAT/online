39297d397784637424f37cf9171eada1475ca1e3
Michael Karrer <michaelkarrer81@gmail.com>
2021-07-01 09:49:55 +0200

    [IMP] partner_twitch field string

--- addons-own/partner_twitch/models/res_partner.py
+++ addons-own/partner_twitch/models/res_partner.py
@@ -6,4 +6,4 @@ from openerp import api, fields, models
 class ResPartnerTwitch(models.Model):
     _inherit = 'res.partner'
 
-    twitch_username = fields.Char(string='twitch user name')
+    twitch_username = fields.Char(string='Twitch User Name')
