c9a48cbba2750cad17789408c3dacf1a8ab026f1
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2021-08-31 17:28:06 +0200

    [IMP] allow update own only for product and template, add tests

--- addons-own/fso_rest_api/tests/test_product_product.py
+++ addons-own/fso_rest_api/tests/test_product_product.py
@@ -9,6 +9,15 @@ logger = logging.getLogger(__name__)
 class TestFsoRestApiProductProduct(FsoRestApiTestCase):
     _model_name = "product.product"
 
+    def create_product_product_via_api(self, code):
+        template_id = self.read_first_from_api(model="product.template")["id"]
+        response = self.create_via_api(data={
+            "product_tmpl_id": template_id,
+            "default_code": code
+        })
+        model = response.json()
+        return int(model["id"])
+
     def test_create_minimal_product_product_works(self):
         expected_code = "Test Code"
         template = self.read_first_from_api(model="product.template")
@@ -29,9 +38,11 @@ class TestFsoRestApiProductProduct(FsoRestApiTestCase):
         model = self.read_first_from_api()
         self.assertModel(model)
 
-    def test_update_product_product_works(self):
-        expected_code = "Test Code"
-        model = self.read_first_from_api()
+    def test_update_own_product_product_works(self):
+        product_id = self.create_product_product_via_api("Test Code")
+
+        expected_code = "New code"
+        model = self.read_from_api(id=product_id)
 
         self.assertNotEqual(model["default_code"], expected_code)
 
@@ -45,6 +56,21 @@ class TestFsoRestApiProductProduct(FsoRestApiTestCase):
 
         actual = self.phantom_env[self._model_name].browse([int(model["id"])])
         self.assertEqual(actual.default_code, expected_code)
+
+    def test_update_others_product_product_is_denied(self):
+        expected_code = "Test Code"
+        model = self.read_first_from_api()
+
+        self.assertModel(model)
+        self.assertNotEqual(model["default_code"], expected_code)
+
+        response = self.update_via_api(data={
+            "id": int(model["id"]),
+            "default_code": expected_code
+        })
+
+        self.assertTrue(not str(response.status_code).startswith("2"))
+        self.assertTrue("Access Denied" in response.content)
 
     def test_delete_product_product_is_denied(self):
         model = self.read_first_from_api()
--- addons-own/fso_rest_api/tests/test_product_template.py
+++ addons-own/fso_rest_api/tests/test_product_template.py
@@ -9,6 +9,13 @@ logger = logging.getLogger(__name__)
 class TestFsoRestApiProductTemplate(FsoRestApiTestCase):
     _model_name = "product.template"
 
+    def create_product_template_via_api(self, name):
+        response = self.create_via_api(data={
+            "name": name
+        })
+        model = response.json()
+        return int(model["id"])
+
     def test_create_minimal_product_template_works(self):
         expected_name = "Test Product Template"
         response = self.create_via_api(data={
@@ -26,9 +33,11 @@ class TestFsoRestApiProductTemplate(FsoRestApiTestCase):
         model = self.read_first_from_api()
         self.assertModel(model)
 
-    def test_update_product_template_works(self):
-        expected_name = "Test Product Template"
-        model = self.read_first_from_api()
+    def test_update_own_product_template_works(self):
+        template_id = self.create_product_template_via_api("Test Product Template")
+
+        expected_name = "New name"
+        model = self.read_from_api(id=template_id)
 
         self.assertNotEqual(model["name"], expected_name)
 
@@ -42,6 +51,21 @@ class TestFsoRestApiProductTemplate(FsoRestApiTestCase):
 
         actual = self.phantom_env[self._model_name].browse([int(model["id"])])
         self.assertEqual(actual.name, expected_name)
+
+    def test_update_others_product_template_is_denied(self):
+        expected_name = "Test Product Template"
+        model = self.read_first_from_api()
+
+        self.assertModel(model)
+        self.assertNotEqual(model["name"], expected_name)
+
+        response = self.update_via_api(data={
+            "id": int(model["id"]),
+            "name": expected_name
+        })
+
+        self.assertTrue(not str(response.status_code).startswith("2"))
+        self.assertTrue("Access Denied" in response.content)
 
     def test_delete_product_template_is_denied(self):
         model = self.read_first_from_api()
