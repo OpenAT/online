caf13b426c7fe931da48e459e86ca43fe64297e5
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2021-08-31 16:33:51 +0200

    [IMP] allow updating/deleting own product.payment_interval_lines only, add tests

--- addons-own/fso_rest_api/tests/test_product_payment_interval_lines.py
+++ addons-own/fso_rest_api/tests/test_product_payment_interval_lines.py
@@ -18,6 +18,20 @@ class TestFsoRestApiProductPaymentInterval(FsoRestApiTestCase):
         })
         return response
 
+    def create_payment_interval_line_as_other(self):
+        product_obj = self.phantom_env["product.product"]
+        interval_obj = self.phantom_env["product.payment_interval"]
+        line_obj = self.phantom_env[self._model_name]
+
+        product = product_obj.search([], order="id", limit=1)
+        interval = interval_obj.search([], order="id", limit=1)
+
+        line = line_obj.sudo().create({
+            "product_id": product.product_tmpl_id.id,
+            "payment_interval_id": interval.id
+        })
+        return line.id
+
     def test_read_product_payment_interval_lines_works(self):
         _ = self.create_payment_interval_line()
 
@@ -28,7 +42,7 @@ class TestFsoRestApiProductPaymentInterval(FsoRestApiTestCase):
         response = self.create_payment_interval_line()
         self.assertEqual(response.status_code, self.HTTP_OK_CREATED)
 
-    def test_update_product_payment_interval_lines_works(self):
+    def test_update_own_product_payment_interval_lines_works(self):
         _ = self.create_payment_interval_line()
 
         expected_sequence = 1001
@@ -46,10 +60,27 @@ class TestFsoRestApiProductPaymentInterval(FsoRestApiTestCase):
         actual = self.phantom_env[self._model_name].browse([int(model["id"])])
         self.assertEqual(actual.sequence, expected_sequence)
 
-    def test_delete_product_payment_interval_lines_works(self):
+    def test_update_others_product_payment_interval_lines_is_denied(self):
+        other_line_id = self.create_payment_interval_line_as_other()
+        expected_sequence = 1001
+
+        response = self.update_via_api(data={
+            "id": other_line_id,
+            "sequence": expected_sequence
+        })
+        self.assertTrue(not str(response.status_code).startswith("2"))
+        self.assertTrue("Access Denied" in response.content)
+
+    def test_delete_own_product_payment_interval_lines_works(self):
         _ = self.create_payment_interval_line()
 
         model = self.read_first_from_api()
         response = self.delete_via_api(model["id"])
         self.assertModel(model)
         self.assertEqual(response.status_code, self.HTTP_OK_NO_CONTENT)
+
+    def test_delete_others_product_payment_interval_lines_is_denied(self):
+        other_line_id = self.create_payment_interval_line_as_other()
+        response = self.delete_via_api(other_line_id)
+        self.assertTrue(not str(response.status_code).startswith("2"))
+        self.assertTrue("Access Denied" in response.content)
