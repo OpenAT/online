f972bb96223c84a29409489fa9ecf15e85901d2f
Michael Karrer <michaelkarrer81@gmail.com>
2021-09-09 10:58:41 +0200

    [payment_consale] extra fields and validation

--- addons-own/payment_consale/models/consale.py
+++ addons-own/payment_consale/models/consale.py
@@ -44,7 +44,7 @@ class PaymentTransactionConsale(models.Model):
                                      string='State')
     consale_transaction_date = fields.Date(string='Payment Transaction Date')
 
-    consale_provider_name = fields.Char(string='Payment Provider Name', help='e.g.: Wirecard, Ogone, ApplePay, ...')
+    consale_provider_name = fields.Char(string='Payment Provider Name', help='e.g.: External Provider Name')
     consale_provider_status = fields.Char(string="Transaction Status Information")
 
     consale_method = fields.Selection(selection=[('paypal', 'PayPal'),
@@ -58,11 +58,25 @@ class PaymentTransactionConsale(models.Model):
     consale_method_other = fields.Char(string='"Other" Payment Method Name')
     consale_method_brand = fields.Char(string='Payment Method Brand', help="e.g.: Visa, Mastercard, Apple, ...")
 
+    # For consale_method 'banktransfer'
+    consale_method_banktransfer_provider = fields.Selection(string="Bank Transfer Payment Provider",
+                                                            selection=[('frst', 'Fundraising Studio'),
+                                                                       ('external', 'External Service')])
+    consale_method_account_owner = fields.Char(string='Account Owner')
+    consale_method_account_iban = fields.Char(string='Account IBAN')
+    consale_method_account_bic = fields.Char(string='Account BIC')
+    consale_method_account_bank = fields.Char(string='Account Bank Name')
+
+    # For recurring payments
+    consale_recurring_payment_provider = fields.Selection(string="Recurring Payment Provider",
+                                                          selection=[('frst', 'Fundraising Studio'),
+                                                                     ('external', 'External Service')])
+
     consale_amount = fields.Float(string="Total Amount")
     consale_currency = fields.Char(string='Currency')
 
     consale_payid = fields.Char(string='PayID')
-    consale_eci = fields.Char(string='Electronic Commerce Indicatior')
+    consale_eci = fields.Char(string='Electronic Commerce Indicator')
     consale_common_name = fields.Char(string='Common Name')
     consale_error_code = fields.Char(string='Error Code')
     consale_error_msg = fields.Char(string='Error Message')
@@ -105,9 +119,34 @@ class PaymentTransactionConsale(models.Model):
                     'consale_method_brand',
                     'consale_amount',
                     'consale_currency']
+
+        # Banktransfer Payment validations
+        if data.get('consale_method', '') == 'banktransfer':
+            consale_method_banktransfer_provider = data.get('consale_method_banktransfer_provider', '')
+            if not consale_method_banktransfer_provider:
+                raise ValidationError("The field 'consale_method_banktransfer_provider' is required for the payment "
+                                      "method 'banktransfer'!")
+            if consale_method_banktransfer_provider == 'frst':
+                required += ['consale_method_account_owner',
+                             'consale_method_account_iban',
+                             'consale_method_account_bank']
+
+        # Recurring Payment validations
+        if data.get('consale_recurring_payment_ever_months', None):
+            consale_recurring_payment_provider = data.get('consale_recurring_payment_provider', None)
+            if not consale_recurring_payment_provider:
+                raise ValidationError("The recurring payment provider 'consale_recurring_payment_provider' must be "
+                                      "set for recurring payments!")
+            if consale_recurring_payment_provider == 'frst':
+                if not data.get('consale_method', '') == 'banktransfer':
+                    raise ValidationError("Only 'banktransfer' method is allowed for recurring payment that should be "
+                                          "executed by Fundraising Studio")
+
+        # Check if any required field value is missing
         missing_fields = tuple(f for f in required if not data.get(f, False))
         if missing_fields:
             raise ValidationError("Fields missing: %s" % missing_fields)
+
         if data.get('consale_method', '') == 'other' and not data.get('consale_method_other', ''):
             raise ValidationError("Field 'consale_method_other' is empty!")
 
