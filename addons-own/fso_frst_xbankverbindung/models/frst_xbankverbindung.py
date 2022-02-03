0d6c76a232b7df69a1e9e4a08f5eed17d128a953
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-02-03 09:31:29 +0100

    [WIP] add frst.xbankverbindung model, link it to loaded

---
+++ addons-own/fso_frst_xbankverbindung/models/frst_xbankverbindung.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class FRSTxBankverbindung(models.Model):
+    """
+    The FundraisingStudio xBankverbindung.
+    """
+    _name = "frst.xbankverbindung"
+
+    beschreibung = fields.Char(string="Beschreibung")
+    kurzbezeichnung = fields.Char(string="Kurzbezeichnung")
