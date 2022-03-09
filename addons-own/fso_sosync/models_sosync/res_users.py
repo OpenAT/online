a575d56353553e7e0509a547a95c002288271796
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-03-09 14:32:54 +0100

    [CHG] comment user and group sync, they have never been implemented and probably will not be

--- addons-own/fso_sosync/models_sosync/res_users.py
+++ addons-own/fso_sosync/models_sosync/res_users.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-'8' "-*-"
-from openerp import models, fields
-
-
-class ResUsersSosync(models.Model):
-    _name = "res.users"
-    _inherit = ["res.users", "base.sosync"]
-
-    # Login name e.g.: "admin" or "max.mustermann@datadialog.net"
-    login = fields.Char(sosync="True")
-
-    # Is the user account enabled or disabled (also hidden in view and searches in FSON if active=False)
-    active = fields.Boolean(sosync="True")
-
-    # Linked res.partner
-    partner_id = fields.Many2one(sosync="True")
-
-    # Access Groups
-    groups_id = fields.Many2many(sosync="True")
-
-    # Last Login
-    login_date = fields.Date(sosync="True")
-
-    # Fundraising Studio Active Directory Name from addon fso_base
-    active_directory_name = fields.Char(sosync="True")
+# from openerp import models, fields
+#
+#
+# class ResUsersSosync(models.Model):
+#     _name = "res.users"
+#     _inherit = ["res.users", "base.sosync"]
+#
+#     # Login name e.g.: "admin" or "max.mustermann@datadialog.net"
+#     login = fields.Char(sosync="True")
+#
+#     # Is the user account enabled or disabled (also hidden in view and searches in FSON if active=False)
+#     active = fields.Boolean(sosync="True")
+#
+#     # Linked res.partner
+#     partner_id = fields.Many2one(sosync="True")
+#
+#     # Access Groups
+#     groups_id = fields.Many2many(sosync="True")
+#
+#     # Last Login
+#     login_date = fields.Date(sosync="True")
+#
+#     # Fundraising Studio Active Directory Name from addon fso_base
+#     active_directory_name = fields.Char(sosync="True")
 
