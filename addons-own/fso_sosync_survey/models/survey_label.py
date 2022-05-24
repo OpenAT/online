50807c0166b3a9f4b5df0ca5460a7494ef19396d
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-05-24 07:42:38 +0200

    [FIX] typo

--- addons-own/fso_sosync_survey/models/survey_label.py
+++ addons-own/fso_sosync_survey/models/survey_label.py
@@ -7,7 +7,7 @@ class SurveyLabelSosync(models.Model):
     _name = 'survey.label'
     _inherit = ['survey.label', 'base.sosync']
 
-    qestion_id = fields.Many2one(sosync="fson-to-frst")
+    question_id = fields.Many2one(sosync="fson-to-frst")
     value = fields.Char(sosync="fson-to-frst")
     sequence = fields.Integer(sosync="fson-to-frst")
     quizz_mark = fields.Float(sosync="fson-to-frst")
